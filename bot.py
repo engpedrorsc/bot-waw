e7e7ef261d2a5501a501dc24c39db04e5d0a80d4
engpedrorsc <60117628+engpedrorsc@users.noreply.github.com>
2021-01-17 01:46:22 -0300

    Code revision
    This revision rewrites code to improve quality and implement new functions, including previous campaigns management.

--- bot.py
+++ bot.py
@@ -1,94 +1,101 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+'''Etapas
+1 - Apresentação e identificação
+2 - Carregar fontes de dados (telefones e mensagem)
+3 - Login
+4 - Envio das mensagens
 
-from pathlib import Path
-from selenium.webdriver.support.ui import WebDriverWait
-from time import time
+'''
+
+
+# from pathlib import Path
+# from selenium.webdriver.support.ui import WebDriverWait
+# from time import time
+# from functions import *
+# from datetime import datetime
+# from urllib.request import urlopen
+# from urllib.error import *
+
 from functions import *
-from datetime import datetime
-from urllib.request import urlopen
-from urllib.error import *
+import shutil
 
 
 def main():
-    input_folder = Path(f'./inputs')
-    log_folder = Path(f'./logs')
+    files = ['mensagem.txt',
+             'telefones_bloqueados.txt',
+             'telefones_para_envio.txt']
+    campaign_folder_root = 'Campanhas'
 
-    print('Assegure-se de que possui o arquivo mais recente de telefones bloqueados e o mova para a pasta "inputs".')
-    print('Download: https://drive.google.com/file/d/1jHOy3IdWb_miNPl0fMlY2WU33_inEn17/view?usp=sharing')
-    input('Pressione ENTER quando estiver atualizado.')
-    black_list_file = input_folder/'bloqueados.txt'
-    if not black_list_file.is_file():
-        raise FileNotFoundError('O arquivo de telefones bloqueados não foi encontrado. Verifique e tente novamente.')
-    
-    print('\nO nome da campanha a identifica de forma única.')
-    print('Para retomar uma campanha interrompida ou para enviá-la para novos clientes, use exatamente o mesmo nome.')
-    print('Os telefones e a mensagem não são recuperados com o nome da campanha. Verifique se estão de acordo.')
-    
-    campaign = ask_campaign_name()
+    presentation('BEM-VINDO  AO  BOT  WAW !')
 
-    log_sent_file = f'{campaign}_sent.txt'
-    log_invalid_file = f'{campaign}_invalid.txt'
-    
-    phones = read_input(input_folder, 'Telefones.txt', 'Lista telefônica carregada.', 'Lista de telefones vazia.') # Phones list.
-    black_list = read_input(input_folder, 'bloqueados.txt', 'Lista telefônica carregada.') # Blacklisted phones
-    message = read_input(input_folder, 'Mensagem.txt', 'Mensagem carregada.', 'Mensagem vazia.') # Message to be sent.
-    sent_log = read_input(log_folder, log_sent_file,'Telefones já contactados carregados.')
-    invalid_log = read_input(log_folder, log_invalid_file, 'Telefones inválidos carregados.')
+    files_ready = input(
+        'Os arquivos com a mensagem e com os telefones podem ser carregados? (s/[n]): ')
+    if files_ready != 's':
+        print('Preencha os arquivos de mensagem e de telefones e inicie novamente.')
+        input('Pressione ENTER para fechar.')
+        exit()
 
-    sent_log_counter = len(sent_log) # Counts previous delivered messages in the current campaign.
-    remaining_phones = difference(phones, black_list, sent_log, invalid_log, 'Telefones da campanha identificados.', 'Sem novos telefones para esta campanha.')
+    campaign_folder = open_campaign(files, campaign_folder_root)
+    os.chdir(campaign_folder)
 
-    driver = open_browser('geckodriver.exe')
-    wdwA = WebDriverWait(driver, 15, poll_frequency=0.5, ignored_exceptions=None) # Short wait.
-    wdwB = WebDriverWait(driver, 30, poll_frequency=0.5, ignored_exceptions=None) # Long wait.
-    max_pace = 90 # Maximum sent messages per hour
+    # log_sent_file = f'{campaign}_sent.txt'
+    # log_invalid_file = f'{campaign}_invalid.txt'
 
-    login(driver, 'http://web.whatsapp.com', wdwA)
-    sent_counter = 0 # Counts current delivered messages in the current campaign.
-    start_time = time() # Set start running time to calculate the pace.
-    for phone in remaining_phones:
-        print(f'Enviando mensagem para {phone}')
-        url = f'https://web.whatsapp.com/send?phone={phone}&source=&data=#.'
-        try:
-            send_message(start_time, driver, url, message, sent_counter, max_pace, wdwA, wdwB)
-        
-        except InvalidUrl:
-            write_log(log_folder, log_invalid_file, phone)
-            continue
+    # phones = read_input(input_folder, 'Telefones.txt', 'Lista telefônica carregada.', 'Lista de telefones vazia.') # Phones list.
+    # black_list = read_input(input_folder, 'bloqueados.txt', 'Lista telefônica carregada.') # Blacklisted phones
+    # message = read_input(input_folder, 'Mensagem.txt', 'Mensagem carregada.', 'Mensagem vazia.') # Message to be sent.
+    # sent_log = read_input(log_folder, log_sent_file,'Telefones já contactados carregados.')
+    # invalid_log = read_input(log_folder, log_invalid_file, 'Telefones inválidos carregados.')
 
-        except WebDriverException:
-            raise WebDriverException('Computador desconectado. Verifique a conexão do computador. (main loop)')
-            input()
-        
-        else:
-            write_log(log_folder, log_sent_file, phone)
-            sent_counter += 1
-            sent_total = sent_counter + sent_log_counter
-            if sent_total == 1:
-                print(f'{sent_total} mensagem enviada nesta campanha.')
-            else:
-                print(f'{sent_total} mensagens enviadas nesta campanha.')
-    
-    show_statistics() # To be written
-    return
+    # sent_log_counter = len(sent_log) # Counts previous delivered messages in the current campaign.
+    # remaining_phones = difference(phones, black_list, sent_log, invalid_log, 'Telefones da campanha identificados.', 'Sem novos telefones para esta campanha.')
 
 
+    # driver = open_browser('geckodriver.exe')
+    # wdwA = WebDriverWait(driver, 15, poll_frequency=0.5, ignored_exceptions=None) # Short wait.
+    # wdwB = WebDriverWait(driver, 30, poll_frequency=0.5, ignored_exceptions=None) # Long wait.
+    # max_pace = 90 # Maximum sent messages per hour
+    # login(driver, 'http://web.whatsapp.com', wdwA)
+    # sent_counter = 0 # Counts current delivered messages in the current campaign.
+    # start_time = time() # Set start running time to calculate the pace.
+    # for phone in remaining_phones:
+    #     print(f'Enviando mensagem para {phone}')
+    #     url = f'https://web.whatsapp.com/send?phone={phone}&source=&data=#.'
+    #     try:
+    #         send_message(start_time, driver, url, message, sent_counter, max_pace, wdwA, wdwB)
+    #     except InvalidUrl:
+    #         write_log(log_folder, log_invalid_file, phone)
+    #         continue
+    #     except WebDriverException:
+    #         raise WebDriverException('Computador desconectado. Verifique a conexão do computador. (main loop)')
+    #         input()
+    #     else:
+    #         write_log(log_folder, log_sent_file, phone)
+    #         sent_counter += 1
+    #         sent_total = sent_counter + sent_log_counter
+    #         if sent_total == 1:
+    #             print(f'{sent_total} mensagem enviada nesta campanha.')
+    #         else:
+    #             print(f'{sent_total} mensagens enviadas nesta campanha.')
+    # show_statistics() # To be written
+    # return
 if __name__ == "__main__":
-    expiration_date = '2020-09-30'
-    expiration_msg = 'Verifique a sua conexão à internet.\nSe estiver OK, contacte o desenvolvedor.'
-    close_msg = 'Pressione ENTER e feche o navegador para encerrar.'
-    try:
-        date_str = urlopen('https://just-the-time.appspot.com/').read().strip().decode('utf-8')
-        date = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-        if date <= datetime.strptime(expiration_date, '%Y-%m-%d'):
-            main()
-        else:
-            print('\n'+expiration_msg)
-            raise Exception(expiration_msg)
-    except URLError or HTTPError or ContentTooShortError:
-        print('\n'+expiration_msg)
-        input('\n'+close_msg)
-    except WebDriverException:
-        print('Ocorreu algum erro com o navegador ou com a conexão do computador.')
-        input('\n'+close_msg)
+    main()
+
+# if __name__ == "__main__":
+#     expiration_date = '2020-09-30'
+#     expiration_msg = 'Verifique a sua conexão à internet.\nSe estiver OK, contacte o desenvolvedor.'
+#     close_msg = 'Pressione ENTER e feche o navegador para encerrar.'
+#     try:
+#         date_str = urlopen('https://just-the-time.appspot.com/').read().strip().decode('utf-8')
+#         date = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+#         if date <= datetime.strptime(expiration_date, '%Y-%m-%d'):
+#             main()
+#         else:
+#             print('\n'+expiration_msg)
+#             raise Exception(expiration_msg)
+#     except URLError or HTTPError or ContentTooShortError:
+#         print('\n'+expiration_msg)
+#         input('\n'+close_msg)
+#     except WebDriverException:
+#         print('Ocorreu algum erro com o navegador ou com a conexão do computador.')
+#         input('\n'+close_msg)
--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+import os
+from datetime import datetime as dt
+import shutil
+from pyfiglet import Figlet, FigletRenderingEngine
 from pathlib import Path
 from time import sleep, time
 from math import ceil
@@ -14,6 +18,7 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 class InvalidUrl(Exception):
     pass
 
+
 class EmptyInputFile(Exception):
     pass
 
@@ -21,20 +26,79 @@ class EmptyInputFile(Exception):
 '''Start and login functions'''
 
 
-def ask_campaign_name():
-    name = input('\n>>>Identifique a campanha<<<\nSugestão: Nome-Data (Ex: Semanal-2020.12.31)\nNome da campanha: ')
-    forbidden = ['<','>',':','"','/','\\','|','?','*']
+def presentation(text: str, style='doom'):
+    redered_text = Figlet(font=style)
+    print('\n' + redered_text.renderText(text))
+
+
+def invalid_char_check(text: str, forbidden_chars: list = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']):
     forbidden_chars_found = []
+    for char in text:
+        if char in forbidden_chars:
+            forbidden_chars_found.append(char)
+    return set(forbidden_chars_found)
+
+
+def backup_campaign(campaign_folder, backup_folder, files):
+    if not os.path.exists(backup_folder):
+        os.makedirs(backup_folder)
+    for file in files:
+        date_time = str(dt.now()).replace(':', '.')
+        shutil.move(src=campaign_folder + os.sep + f'{file}',
+                    dst=backup_folder + os.sep + f'{date_time}_{file}')
+
+
+def move_campaign_files(campaign_folder, files):
+    for file in files:
+        shutil.copy(src=f'{file}', dst=campaign_folder)
+
+
+def open_mode():
+    print('\nJá existe uma campanha com este nome.')
+    print('Escolha uma das opções abaixo:')
+    print('1 - Criar nova campanha com novo nome.')
+    print('2 - Continuar a campanha já existente com os arquivos originais.')
+    print('3 - Continuar a campanha já existente com os novos arquivos.')
+    choice = int(input('Digite a opção escolhida e pressione ENTER: '))
+    if choice not in [1, 2, 3]:
+        print('\n---> Opção inválida.')
+        open_mode()
+    return choice
+
+
+def open_campaign(files, campaign_folder_root):
+    print('\n>>> Identifique a campanha <<<')
+    print('Sugestão: Nome-Data (Ex: Semanal-2020.12.31)')
+    name = input('Nome da campanha: ')
+
     if len(name) == 0:
         print('\nO nome da campanha é obrigatório.')
-        return ask_campaign_name()
-    for char in name:
-        if char in forbidden:
-            forbidden_chars_found.append(char)
-    if len(forbidden_chars_found) > 0:        
-        print(f'\nErro: O nome escolhido possui os seguintes caracteres inválidos: {forbidden_chars_found}')        
-        return ask_campaign_name()
-    return name
+        return open_campaign(files)
+
+    forbidden_chars_found = invalid_char_check(name)
+    if len(forbidden_chars_found) > 0:
+        found = ' '.join(forbidden_chars_found)
+        print(
+            f'\nErro: O nome escolhido possui os seguintes caracteres inválidos: {found}')
+        return open_campaign(files)
+
+    campaign_folder = campaign_folder_root + os.sep + name
+    if not os.path.exists(campaign_folder):
+        os.makedirs(campaign_folder)
+        move_campaign_files(campaign_folder, files)
+    else:
+        choice = open_mode()
+        if choice == 1:
+            print('Insira um nome de campanha que ainda não exista.')
+            open_campaign(files, campaign_folder_root)
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            backup_folder = campaign_folder + os.sep + 'Backup'
+            backup_campaign(campaign_folder, backup_folder, files)
+            move_campaign_files(campaign_folder, files)
+
+    return campaign_folder
 
 
 def open_browser(driver):
@@ -46,14 +110,16 @@ def open_browser(driver):
 def login(driver, url, wdw):
     try:
         driver.get(url)
-        wdw.until(presence_of_element_located((By.XPATH, '//*[@aria-label = "Scan me!"]')))
+        wdw.until(presence_of_element_located(
+            (By.XPATH, '//*[@aria-label = "Scan me!"]')))
         print('Escaneie o código QR para iniciar o WhatsApp Web.')
     except TimeoutException:
         # driver.refresh()
         return login(driver, url, wdw)
-    
+
     try:
-        wdw.until(presence_of_element_located((By.XPATH, '//span[@data-testid = "laptop"]')))
+        wdw.until(presence_of_element_located(
+            (By.XPATH, '//span[@data-testid = "laptop"]')))
     except TimeoutException:
         # driver.refresh()
         return login(driver, wdw)
@@ -65,7 +131,7 @@ def login(driver, url, wdw):
 
 
 def read_input(path, file, msg, msg_error=''):
-    path.mkdir(parents = True, exist_ok = True)
+    path.mkdir(parents=True, exist_ok=True)
     try:
         f = open(path/file, 'rb')
     except FileNotFoundError:
@@ -86,7 +152,7 @@ def write_log(path, file, text):
     f.close()
 
 
-def difference(full_list, list1, list2, list3, msg): # To be improved
+def difference(full_list, list1, list2, list3, msg):  # To be improved
     filtered = set(full_list) - set(list1) - set(list2) - set(list3)
     print(msg)
     return list(filtered)
@@ -118,28 +184,32 @@ def send_message(start_time, driver, url, message, sent_counter, max_pace, wdw1,
         text_box = driver.find_element(locator[0], locator[1])
         for line in message:
             text_box.send_keys(line)
-            ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.SHIFT).key_up(Keys.ENTER).perform()
+            ActionChains(driver).key_down(Keys.SHIFT).key_down(
+                Keys.ENTER).key_up(Keys.SHIFT).key_up(Keys.ENTER).perform()
         check_pace(max_pace, start_time, sent_counter)
         # input('Pressione ENTER para enviar a mensagem.')
         text_box.send_keys(Keys.ENTER)
-        wdw2.until_not(presence_of_all_elements_located((By.XPATH, '//span[@data-testid = "msg-time"]')))
-    
+        wdw2.until_not(presence_of_all_elements_located(
+            (By.XPATH, '//span[@data-testid = "msg-time"]')))
+
     except TimeoutException:
         if bool(driver.find_elements(By.XPATH, '//*[contains(text(), "Phone number shared via url is invalid.") or\
                                                     contains(text(), "O número de telefone compartilhado através de url é inválido.")]')):
             print('URL inválida.')
             raise InvalidUrl
         elif bool(driver.find_elements(By.XPATH, '//*[contains(text(), "Trying to reach phone") or\
-                                                      contains(text(), "Tentando conectar ao celular")]')) or\
-             bool(driver.find_elements(By.XPATH, '//span[@data-testid = "alert-phone"]')):
-            print('Telefone desconectado. Verifique a conexão do telefone. (send_message)')
+                                                          contains(text(), "Tentando conectar ao celular")]')) or\
+                bool(driver.find_elements(By.XPATH, '//span[@data-testid = "alert-phone"]')):
+            print(
+                'Telefone desconectado. Verifique a conexão do telefone. (send_message)')
             return send_message(start_time, driver, url, message, sent_counter, max_pace, wdw1, wdw2)
         elif bool(driver.find_elements(By.XPATH, '//*[@aria-label = "Scan me!"]')):
             print('Refaça o login. (send_message)')
             login(driver, wdw1)
             return send_message(start_time, driver, url, message, sent_counter, max_pace, wdw1, wdw2)
         elif bool(driver.find_elements(By.XPATH, '//span[@data-testid = "alert-computer"]')):
-            print('Computador desconectado. Verifique a conexão do computador. (send_message)')
+            print(
+                'Computador desconectado. Verifique a conexão do computador. (send_message)')
             return send_message(start_time, driver, url, message, sent_counter, max_pace, wdw1, wdw2)
         else:
             driver.refresh()
